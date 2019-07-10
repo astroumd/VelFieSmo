f2c90b83ce00891521a4f0c330a90229f82f222e
Peter Teuben <teuben@gmail.com>
2019-07-10 14:54:58 -0400

    fixed up labeling and comments

--- scripts/smooth_inc.py
+++ scripts/smooth_inc.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python
 #
-#   effect  of beam smearing on inclination
+#   effect of beam smearing on inclination
+#   this plots how the kinematic inclination changed
+#
+#   Notes:
+#      - residual maps have a 3-theta pattern
+#      - solutions not really much different for m=1 and m=3
+#      - Re has big influence, for smaller Re INC changes a lot
+#
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# data are taken from rotcurshape on variations on tghe following
-# rm -rf model0; ../scripts/mkgalcube m=2 beam=1 inc=75 re=1 vel=mom
+# data are taken from rotcurshape on variations on the following command:
+#   rm -rf model0; ../scripts/mkgalcube m=2 beam=1 inc=75 re=1 vel=mom
 
 i1  = np.array([60,   65,   70,   75,   80])
 i1f = np.array([58.3, 63.0, 67.5, 71.7, 75.1])
@@ -29,14 +35,14 @@ i4f = np.array([66.3])
 di4 = i4-i4f
 
 
-plt.plot(i1,       di1, label='re=2')
-plt.scatter(i2,    di2, label='re=1000')
-plt.scatter(i3,    di3, label='re=1')
-plt.scatter(i4,    di4, label='re=0.5')
+plt.plot(i1,       di1, label='$R_e$=2')
+plt.scatter(i2,    di2, label='$R_e$=1000')
+plt.scatter(i3,    di3, label='$R_e$=1')
+plt.scatter(i4,    di4, label='$R_e$=0.5')
 
 plt.title('Kinematic Inclination; m=2')
-plt.xlabel('INC')
-plt.ylabel('INC-FIT')
+plt.xlabel('$i_{morph}$')
+plt.ylabel('$i_{morph} - i_{kinem}$')
 plt.legend(loc='upper left')
 plt.grid()
 plt.savefig('smooth_inc.png')
