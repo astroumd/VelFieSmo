35835c8f87b306d981f1f5b6a9325fb84c14099c
Peter Teuben <teuben@gmail.com>
2019-08-02 09:46:54 -0400

    use  a function, standardize filename

--- scripts/model_rot_curve.py
+++ scripts/model_rot_curve.py
@@ -1,20 +1,25 @@
 #! /usr/bin/env python
+#
 ##plotting exercise
 ##created by Nate
 ##06/24/2019
 import numpy as np
 import matplotlib.pyplot as plt
 
+def vrot(x, m):
+    return x / pow(1+x**m, 1/m)
+
 x =np.linspace(0,10,1000)
-v1 = x/(1+x)
-v1_5 = x/((1+x**(3/2))**(1/(3/2)))
-v2 = x/((1+x**2)**(1/2))
-v4 = x/((1+x**4)**(1/4))
-v10 = x/((1+x**10)**(1/10))
-v25 = x/((1+x**25)**(1/25))
-v100 = x/((1+x**100)**(1/100))
+
 ve = 1-np.exp(-x)
 
+v7 = vrot(x,0.75)
+v1 = vrot(x,1)
+v2 = vrot(x,2)
+v4 = vrot(x,4)
+v100 = vrot(x,100)
+
+#plt.plot(x, v7, '-r', label='m=0.75')
 plt.plot(x, v1, '-r', label='m=1')
 #plt.plot(x, v1_5, '-m', label='m=1.5')
 plt.plot(x, v2, '-k', label='m=2')
@@ -24,11 +29,11 @@ plt.plot(x, v4, '-b', label='m=4')
 plt.plot(x, v100, '-g', label='m=100')
 plt.plot(x, ve, '--m', label='exp')
 
-plt.title('Model Rotation Curves')
-plt.xlabel('$R/R_O$', color='#1C2833')
-plt.ylabel('$V/V_O$', color='#1C2833')
+plt.title('Model Rotation Curves: $V/V_0 = r / (1+r^m)^{1/m},   r=R/R_0$')
+plt.xlabel('$R/R_O$')
+plt.ylabel('$V/V_O$') 
 plt.legend(loc='lower right')
 plt.grid()
-plt.savefig('model_rotcur.png')
+plt.savefig('model_rot_curve.pdf')
 plt.show()
 
